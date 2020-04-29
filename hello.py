99b8b6b2d3b64757de9d6c7adb9c851f22e5557f
Sumanta Mandal <mr.sumantamandal@gmail.com>
2020-04-29 23:57:59 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -281,22 +281,6 @@ c = 500
 if a > b or a > c:
   print("At least one of the conditions is True")'''
 
-  x = 41
-
-if x > 10:
-  print("Above ten,")
-  if x > 20:
-    print("and also above 20!")
-  else:
-    print("but not above 20.")
-
-
- i = 1
-while i < 6:
-  print(i)
-  if i == 3:
-    break
-  i += 1
 
 
 
