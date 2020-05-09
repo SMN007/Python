fbe607f9966b9e008619e6c74ee3be143b36c731
Sumanta Mandal <mr.sumantamandal@gmail.com>
2020-05-09 23:59:16 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -257,6 +257,13 @@ if "model" in thisdict:
   print("Yes, 'model' is one of the keys in the thisdict dictionary")
 """
 
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
 
 
 
